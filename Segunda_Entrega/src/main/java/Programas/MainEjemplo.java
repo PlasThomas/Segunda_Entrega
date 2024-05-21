edb1f5746a60ce1ad8457384131615a84ccb023f
PlasThomas <joseplascencia346@aragon.unam.mx>
2024-05-21 07:51:05 -0600

    ASDP

--- Segunda_Entrega/src/main/java/Programas/MainEjemplo.java
+++ Segunda_Entrega/src/main/java/Programas/MainEjemplo.java
@@ -6,9 +6,15 @@ import java.util.ArrayList;
 
 public class MainEjemplo {
     public static void main(String[] args) throws IOException {
+        ArrayList<String> out = new ArrayList<String>();
+        /*
         BnfRec bnfr = new BnfRec();
         bnfr.setArchivo("src/main/java/Programas/fuente.txt");
-        ArrayList<String> out = bnfr.task();
+        ArrayList<String> out = bnfr.task():
+        */
+        ASDP asdp = new ASDP();
+        asdp.setArchivo("src/main/java/Programas/fuente.txt");
+        out = asdp.task();
         for (String s : out) System.out.println(s);
     }
 }
